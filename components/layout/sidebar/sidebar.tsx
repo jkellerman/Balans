f38b1b0e662bdb5fe4150cc480ca841b50e458da
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-06-10 23:42:02 +0100

    chore: change width of sidebar for better ui

--- components/layout/sidebar/sidebar.tsx
+++ components/layout/sidebar/sidebar.tsx
@@ -6,7 +6,7 @@ import Navigation from "./navigation";
 
 export default function Sidebar() {
 	return (
-		<div className="hidden w-[120px] min-w-[120px] flex-col items-center border-r border-border py-8 transition-colors md:flex">
+		<div className="hidden w-[100px] min-w-[100px] flex-col items-center border-r border-border py-8 transition-colors md:flex">
 			<div className="mb-10">
 				<Logo />
 			</div>
