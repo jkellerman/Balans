bcd980a1b22ab8ed47275f9fe91a671a40d5df03
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-06-10 13:33:29 +0100

    chore: add global types

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+export declare namespace Types {
+	export type Icons =
+		| "ArrowUp"
+		| "Calendar"
+		| "ChevronRight"
+		| "Expenses"
+		| "Help"
+		| "Home"
+		| "Income"
+		| "Investments"
+		| "Piggy"
+		| "Settings"
+		| "Spaces"
+		| "Subscriptions"
+		| "Toggle"
+		| "Transactions"
+		| "Wallet";
+}
