c3688923d79876373bf031038f168df129b3f594
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-06-12 00:23:35 +0100

    chore: minor changes

--- components/layout/sidebar/sidebar.tsx
+++ components/layout/sidebar/sidebar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 import Logo from "@/components/logo";
 
 import Navigation from "./navigation";
@@ -8,7 +10,13 @@ export default function Sidebar() {
 	return (
 		<div className="hidden w-[100px] min-w-[100px] flex-col items-center border-r border-border py-8 transition-colors md:flex">
 			<div className="mb-10">
-				<Logo />
+				<Link href="/">
+					<span aria-hidden="true">
+						<Logo />
+					</span>
+
+					<span className="sr-only">Home</span>
+				</Link>
 			</div>
 			<Navigation />
 		</div>
