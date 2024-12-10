59514c5311f6f9a6cb54677ea6b455149fbabc64
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-12-10 10:49:15 +0000

    feat: create redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import ThemeProvider from "@/components/theme";
 
 export default function Home() {
+	redirect("/dashboard");
 	return (
 		<main className="flex flex-col items-center justify-between gap-10 p-24">
 			<Link href="/dashboard" className="text-lg font-bold capitalize lg:text-xl">
