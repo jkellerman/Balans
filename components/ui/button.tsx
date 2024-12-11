8f4914dbfa555771c609ea5172e0662f5c5d2b29
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-12-11 21:04:18 +0000

    add primary button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,13 +7,14 @@ import { VariantProps, cva } from "class-variance-authority";
 const buttonVariants = cva("inline-flex items-center justify-center rounded-md ", {
 	variants: {
 		variant: {
-			primary: "",
+			primary:
+				"relative flex h-[50px] w-40 items-center justify-center overflow-hidden bg-primary text-quinary shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-foreground before:duration-500 before:ease-out hover:shadow-secondary hover:before:h-56 hover:before:w-56",
 			secondary: "",
 			destructive: "",
 			ghost: "text-tertiary",
 		},
 		size: {
-			default: "px-5 py-2 text-sm",
+			default: "px-5 py-2 text-sm font-bold",
 			sm: "px-3 py-2",
 			md: "px-4 py-2",
 			none: "p-0",
