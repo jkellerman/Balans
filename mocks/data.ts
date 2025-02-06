edf5efb1a7298f13103e63159975ab3f4cf25d9a
jkellerman <89940123+jkellerman@users.noreply.github.com>
2025-02-06 16:30:29 +0000

    refactor: add upcoming payment

--- mocks/data.ts
+++ mocks/data.ts
@@ -1,6 +1,7 @@
 export const upcomingPayments = [
 	{ name: "Phone", date: "27th Jun 2024", cost: 12.86 },
 	{ name: "Rent", date: "27th Jun 2024", cost: 980.2 },
+	{ name: "Spotify", date: "29th Jun 2024", cost: 9.99 },
 ];
 
 export const recentTransactions = [
