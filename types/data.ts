783c4ecc9c0ff3623e8d4ce80111a8f220e5cd2a
jkellerman <89940123+jkellerman@users.noreply.github.com>
2025-02-10 12:51:10 +0000

    refactor: update data types

--- types/data.ts
+++ types/data.ts
@@ -1,3 +1,5 @@
+export type Interval = "weekly" | "monthly" | "yearly";
+
 export interface FinancialEntity {
 	id: number;
 	name: string;
@@ -6,14 +8,29 @@ export interface FinancialEntity {
 	date: Date;
 }
 
+export interface RecurringPayment {
+	interval: Interval;
+	active: boolean;
+}
+
 export interface Transaction extends FinancialEntity {
 	type: "income" | "expense";
+	recurring?: RecurringPayment;
+}
+
+export interface Subscription extends FinancialEntity {
+	type: "subscription";
+	recurring: RecurringPayment;
+}
+
+export interface Investment extends FinancialEntity {
+	type: "investment";
 }
 
 export interface Data {
 	transactions: Transaction[];
-	subscriptions: FinancialEntity[];
-	investments: FinancialEntity[];
+	subscriptions: Subscription[];
+	investments: Investment[];
 }
 
 export type Month = "Jan" | "Feb" | "Mar" | "Apr" | "May" | "Jun" | "Jul" | "Aug" | "Sep" | "Oct" | "Nov" | "Dec";
